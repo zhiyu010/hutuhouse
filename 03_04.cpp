9ee2ee25de37dd430892af8e09927828842e41e3
zhiyu <1078602615@qq.com>
2023-10-20 11:43:19 +0800

    03_04

--- 03_04.cpp
+++ 03_04.cpp
@@ -4,17 +4,22 @@ using namespace std;
 int main()
 {
     int i = 0;
-    int o;
-    cout << setw(10) << "*" << endl;
-    while (i<9)
+    int o,w;
+    cout << "请输入需要的行数";
+    cin >> w;
+    cout << setw(w) << "*" << endl;
+    while (i<w-1)
     {
         i++;
-        cout << setw(10 - i)<<"*";
+        cout << setw(w - i)<<"*";
         for(o=1;o <= 2*i-1;o++){
             cout<<" ";
         }
         cout << "*" << endl;
 
+    }
+    for (o=1;o <= 2*w;o++){
+        cout<<"*";
     }
     return 0;
 
